de56b274d55bdced90da249a7004cfe6fcc5dc9d
Curtis Chong <curtischong5@gmail.com>
2019-01-31 20:27:11 -0500

    Initial Commit

---
+++ lizzie_peaks/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  lizzie_peaks
+//
+//  Created by Curtis Chong on 2019-01-31.
+//  Copyright © 2019 Curtis Chong. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
