f460ef17d2a711f7fe8c5127db1ae54be9eaba41
Curtis Chong <curtischong5@gmail.com>
2019-02-02 01:57:08 -0500

    added picker

--- lizzie_peaks/SettingsViewController.swift
+++ lizzie_peaks/SettingsViewController.swift
@@ -8,14 +8,40 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController , UIPickerViewDelegate, UIPickerViewDataSource{
 
+    @IBOutlet weak var defaultReviewPicker: UIPickerView!
+    var pickerData: [String] = [String]()
     let dataManager = DataManager()
+    var defaultReviewCurve : Int!
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        defaultReviewPicker.delegate = self
+        defaultReviewPicker.dataSource = self
+        pickerData = ["Simple", "Moderate", "ML"]
+        defaultReviewCurve = 0
     }
+    
+    // Number of columns of data
+    func numberOfComponents(in pickerView: UIPickerView) -> Int{
+    return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int)-> Int {
+    return pickerData.count
+    }
+    
+    private func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String{
+        return pickerData[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent  component: Int) {
+        let yearValueSelected = pickerData[row] as String
+        print("selected: \(yearValueSelected)")
+    }
+    
 
     @IBAction func backSettingsBtn(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
