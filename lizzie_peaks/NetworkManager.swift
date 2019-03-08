5bfae69b3e13b68ddea82209ba502270b4968f9f
Curtis Chong <curtischong5@gmail.com>
2019-03-08 00:29:09 -0500

    working on added scheduled reviews to server

--- lizzie_peaks/NetworkManager.swift
+++ lizzie_peaks/NetworkManager.swift
@@ -9,28 +9,56 @@
 import Foundation
 import Alamofire
 class NetworkManager{
-    let SERVERIP = "http://10.8.0.2:9000"
-    let sendToServer = false
+    let SERVERIP = "http://10.8.0.1:9000"
+    let sendToServer = true
+    
+    func convertDate(date : Date) -> String{
+        return String(Double(round(1000*date.timeIntervalSince1970)/1000))
+    }
+    
     func uploadSkill(skill : SkillObj){
         if(!sendToServer){
             NSLog("network turned off")
             return
         }
+        
+        
         let parameters: Parameters = [
             "concept" : skill.concept,
             "newLearnings" : skill.newLearnings,
             "oldSkills" : skill.oldSkills,
-            "percentNew" : skill.percentNew,
-            "timeLearned" : skill.timeLearned,
-            "timeSpentLearning" : skill.timeSpentLearning,
-            "scheduledReviews" : skill.scheduledReviews,
-            "scheduledReviewDurations" : skill.scheduledReviewDurations,
-            "reviews" : skill.reviews,
-            "reviewDurations" : skill.reviewDurations
+            "percentNew" : String(skill.percentNew),
+            "timeLearned" : convertDate(date: skill.timeLearned),
+            "timeSpentLearning" : String(skill.timeSpentLearning)
         ]
         
         //TODO: change the ip to the ip of the server
         AF.request(SERVERIP + "/upload_skill",
+                   method: .post,
+                   parameters: parameters,
+                   encoding: JSONEncoding()
+            ).responseJSON { response in
+                
+                print("Request: \(String(describing: response.request))")   // original url request
+                print("Response: \(String(describing: response.response))") // http url response
+                print("Result: \(response.result)")                         // response serialization result
+        }
+    }
+    
+    func uploadScheduledReview(concept: String, timeLearned: Date, reviewDate: Date, reviewDuration: Int){
+        if(!sendToServer){
+            NSLog("network turned off")
+            return
+        }
+        let parameters: Parameters = [
+            "concept" : concept,
+            "timeLearned" : convertDate(date: timeLearned),
+            "scheduledDate" : convertDate(date: reviewDate),
+            "scheduledDuration" : String(reviewDuration)
+        ]
+        
+        //TODO: change the ip to the ip of the server
+        AF.request(SERVERIP + "/upload_scheduled_review",
                    method: .post,
                    parameters: parameters,
                    encoding: JSONEncoding()
